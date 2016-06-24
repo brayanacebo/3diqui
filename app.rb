870e3ca0067df879ddb8538edcc4b54f74ed09c4
Brayan Acebo <brayanacebo@gmail.com>
2016-06-24 14:42:49 -0500

    ya hace swich

--- app.rb
+++ app.rb
@@ -1,12 +1,22 @@
 require 'sinatra'
 
-
+enable :sessions
+#session[:turno] = "O"
 
 get '/' do
     erb :index
 end
 
 post '/' do
-	@marcao = "O"
+	@marca = session[:turno]
+
+	if session[:turno] == "X"
+		@marca = "X"
+		session[:turno] = "O"
+	else
+		@marca = "O"
+		session[:turno] = "X"
+	end
+
 	erb :index
 end
